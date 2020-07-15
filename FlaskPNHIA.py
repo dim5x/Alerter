c281a43c01572da318719960f91e65607f305e54
dim5x <dim5x@yahoo.com>
2020-07-15 19:20:22 +0300

    Кнопки добавления мак-адресов и страницы заглушки для этого.

--- FlaskPNHIA.py
+++ FlaskPNHIA.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, redirect, g
+from flask import Flask, render_template
 import datetime
 
-allowMAC=['84:85:06:21:d5:5a']
-disallowMAC=['00:25:06:11:d5:00','08:75:56:11:11:11']
+allowMAC = ['84:85:06:21:d5:5a']
+disallowMAC = ['00:25:06:11:d5:00', '08:75:56:11:11:11']
 
 app = Flask(__name__)
 
@@ -12,7 +12,7 @@ def hello_world():
     today = datetime.datetime.today()
     name_syslog_file = today.strftime('%Y-%m-%d') + '.log'
     with open(name_syslog_file, 'r') as f:
-        s = f.readlines()
+        s = reversed(f.readlines())
     return render_template('index.html', data=s, allowMAC=allowMAC, disallowMAC=disallowMAC)
 
 
