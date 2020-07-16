6a81ae791f8c11ba4fbd7ff0e0a9872a77dfaf48
dim5x <dim5x@yahoo.com>
2020-07-16 22:05:06 +0300

    PEP 8.

--- FlaskPNHIA.py
+++ FlaskPNHIA.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 import datetime
 
 # Тестовые МАК адреса для отображения:
-allowMAC = ['84:85:06:21:d5:5a']
-disallowMAC = ['00:25:06:11:d5:00', '08:75:56:11:11:11']
+allow_mac = ['84:85:06:21:d5:5a']
+disallow_mac = ['00:25:06:11:d5:00', '08:75:56:11:11:11']
 
 app = Flask(__name__)
 
@@ -15,19 +15,19 @@ def hello_world():
     name_syslog_file = today.strftime('%Y-%m-%d') + '.log'
     with open(name_syslog_file, 'r') as f:
         s = reversed(f.readlines())
-    return render_template('index.html', data=s, allowMAC=allowMAC, disallowMAC=disallowMAC)
+    return render_template('index.html', data=s, allow_mac=allow_mac, disallow_mac=disallow_mac)
 
 
 # Заглушка страницы добавления.
-@app.route('/add_allowMAC', methods=['POST', 'GET'])
-def add_allowMAC():
-    return render_template('add_allowMAC.html', allowMAC=allowMAC)
+@app.route('/add_allow_mac', methods=['POST', 'GET'])
+def add_allow_mac():
+    return render_template('add_allow_mac.html', allow_mac=allow_mac)
 
 
 # Заглушка страницы добавления.
 @app.route('/add_disallowMAC', methods=['POST', 'GET'])
-def add_disallowMAC():
-    return render_template('add_disallowMAC.html', disallowMAC=disallowMAC)
+def add_disallow_mac():
+    return render_template('add_disallow_mac.html', disallow_mac=disallow_mac)
 
 
 if __name__ == '__main__':
