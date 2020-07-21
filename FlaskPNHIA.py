8749189dba9b193ff78c09b0f00baa5d3a4ab20d
dim5x <dim5x@yahoo.com>
2020-07-21 06:59:18 +0300

    Добавлено: - настройка Flask, указана папка static - почему-то только из неё удаётся запустить bootstrap.css и иконку без использования костылей; - функционал добавления мак-адресов; - редирект на главную после добавления; - обновление инф.на главной о внесённых добавлениях;

--- FlaskPNHIA.py
+++ FlaskPNHIA.py
@@ -1,37 +1,56 @@
-from flask import Flask, render_template
-import datetime
-
-# Тестовые МАК адреса для отображения:
-allow_mac = ['84:85:06:21:d5:5a']
-disallow_mac = ['00:25:06:11:d5:00', '08:75:57:11:11:11']
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+app.static_folder = r'templates\static'
 
 
 # Главная страница.
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
-    today = datetime.datetime.today()
-    name_syslog_file = today.strftime('%Y-%m-%d') + '.log'
+    name_syslog_file = 'sys.log'
+    global allow_mac
+    global disallow_mac
     try:
-        with open(name_syslog_file, 'r') as f:
-            s = reversed(f.readlines())
-        return render_template('index.html', data=s, allow_mac=allow_mac, disallow_mac=disallow_mac)
+        with open(name_syslog_file, 'r') as f, \
+                open('allow_mac.txt', 'r') as am, \
+                open('disallow_mac.txt', 'r') as dm:
+            data = reversed(f.readlines())
+            allow_mac = am.readlines()
+            disallow_mac = dm.readlines()
+        return render_template('index.html', data=data, allow_mac=allow_mac, disallow_mac=disallow_mac)
     except FileNotFoundError:
         return render_template('FileNotFoundError.html')
 
+
 # Заглушка страницы добавления.
-@app.route('/add_allow_mac', methods=['POST', 'GET'])
+@app.route('/add_allow_mac.html', methods=['POST', 'GET'])
 def add_allow_mac():
+    global allow_mac
+    if request.method == 'POST':
+        if request.form['button'] == 'Добавить':
+            with open('allow_mac.txt', 'a') as f:
+                mac = request.form['field']
+                f.write(mac + '\n')
+            return redirect('/')
     return render_template('add_allow_mac.html', allow_mac=allow_mac)
 
 
 # Заглушка страницы добавления.
-@app.route('/add_disallow_mac', methods=['POST', 'GET'])
+@app.route('/add_disallow_mac.html', methods=['POST', 'GET'])
 def add_disallow_mac():
+    global allow_mac
+    if request.method == 'POST':
+        if request.form['button'] == 'Добавить':
+            with open('disallow_mac.txt', 'a') as f:
+                mac = request.form['field']
+                f.write(mac + '\n')
     return render_template('add_disallow_mac.html', disallow_mac=disallow_mac)
 
 
+# @app.route('/allow_mac.txt')
+# def txt():
+#     return render_template('allow_mac.txt')
+
+
 if __name__ == '__main__':
-    app.run()
-# todo FileNotFoundError Если нет файла лога.
+    app.run(debug=True, use_reloader=True)
