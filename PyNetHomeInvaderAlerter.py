097c057557536bed72b280c32135dfa1442c23a8
dim5x <dim5x@yahoo.com>
2020-07-21 07:00:51 +0300

    Запись в sys.log вместо разных файлов каждый день.

--- PyNetHomeInvaderAlerter.py
+++ PyNetHomeInvaderAlerter.py
@@ -3,14 +3,14 @@ import socketserver
 
 # HOST, PORT = 'x.x.x.x', 514
 # HOST, PORT = 'localhost', 514
-HOST, PORT = '192.168.0.3', 514
+HOST, PORT = '192.168.2.2', 514
 
 
 class SyslogUDPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         data = bytes.decode(self.request[0].strip())
         today = datetime.datetime.today()
-        name_syslog_file = today.strftime('%Y-%m-%d') + '.log'
+        name_syslog_file = 'sys.log'
         with open(name_syslog_file, 'a') as f:
             if 'kernel: wlan0' in str(data):
                 f.write('IP: ' + self.client_address[0] + '\t' + today.strftime('%Y-%m-%d %H:%M:%S') + '\t' + str(
