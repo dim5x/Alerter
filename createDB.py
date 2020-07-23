e9069626530e337056a9ff68925b922ffa99242f
dim5x <dim5x@yahoo.com>
2020-07-23 19:00:53 +0300

    Файлик по созданию табличек БД.

---
+++ createDB.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+""" Создал табличку c именем syslog и полями: 
+                            id
+                            priority = приоритет
+                            event_time = время события
+                            fromhost = IP-адресс (доменное имя) 
+                            syslogtag = источник события
+                            message = сообщение о событии
+                            mac_allow = разрешенные маки
+                            mac_disallow = неразрешенные маки
+                            rez = резерв
+                            """
+db = sqlite3.connect('destination.db')  # создаём коннект с базой
+cursor = db.cursor()  # создаём курсор
+
+# SQL- запрос на создание:
+cursor.execute('''CREATE TABLE IF NOT EXISTS syslog (id INTEGER PRIMARY KEY,
+                        priority integer,
+                        event_time DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%S','now','localtime')),
+                        fromhost varchar(200),
+                        syslogtag varchar(50),
+                        event varchar(400),
+                        mac_allow varchar(17),
+                        mac_disallow varchar(17),
+                        rez text)''')
+db.commit()
+db.close()
