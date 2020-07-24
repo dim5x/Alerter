04e9ab910dcceabd307ce7d2bf9e19f987d6c818
dim5x <dim5x@yahoo.com>
2020-07-24 20:13:52 +0300

    1. Ужал строки по PEP8. 2. Убрал подкласс. 3. Добавил 'r'. 4. Добавил для себя #fromhost = self.client_address[0]

--- PyNetHomeInvaderAlerter.py
+++ PyNetHomeInvaderAlerter.py
@@ -6,60 +6,52 @@ import datetime
 # Под виндой с 514-ым портом могут быть проблемы, нужно повышение привилегий.
 # Заменить тем, что выше 1023-его.
 # HOST, PORT = 'x.x.x.x', 514
-#HOST, PORT = '192.168.0.102', 5140
+HOST, PORT = '192.168.0.102', 5140
 # my_branch test
-HOST, PORT = '172.27.0.165', 514
+# HOST, PORT = '172.27.0.165', 514
 
-""" Запись syslog'a в таблицу: 
-        priority
-	devicereportedtime
-        receivedat
-        fromhost
-        process
-        syslogtag
-        message
-"""
-
-class MyUDPServer(socketserver.UDPServer):
-    def __init__(self, server_address, RequestHandlerClass, db, bind_and_activate=True):
-        socketserver.UDPServer.__init__(self, server_address, RequestHandlerClass)
-        self.db = db
-    def server_close(self):
-        socketserver.UDPServer.server_close()
-        db.close()
+db = sqlite3.connect('destination.db')  # создаём коннект с базой
+cursor = db.cursor()  # создаём курсор
 
 
 class SyslogUDPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         data = bytes.decode(self.request[0].strip())
-        cursor = self.server.db.cursor()  # создаём курсор
+
+        # Parse
+        event = re.search(
+            r'(?P<priority><\d{,3}>)(?P<date>\w{,3}\s+\d{,2}\s+\d{,2}:\d{,2}:\d{2,2})(?P<fromhost>\s+[^:]+){0,'
+            r'1}\s+(?P<process>\S+:)(?P<syslogtag>\s+\S+:){0,1}\s+(?P<message>.+)', data)
+        priority = event.group('priority').replace('<', '').replace('>', '')
+        timestamp = datetime.datetime.strptime(str(datetime.datetime.now().year) + ' ' + event.group('date'),
+                                               '%Y %b %d %H:%M:%S')
+        devicereportedtime = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+        # fromhost = self.client_address[0]
+        fromhost = event.group('fromhost')
+        process = event.group('process')
+        syslogtag = event.group('syslogtag')
+        message = event.group('message')
+        cursor.execute(
+            "INSERT INTO syslog (priority,devicereportedtime,fromhost,process,syslogtag,message) VALUES (?,?,?,?,?,?)",
+            (priority, devicereportedtime, fromhost, process, syslogtag, message))
+
+        db.commit()
 
         # for debug
         '''
         with open('sys.log', 'a') as f:
             f.write(data+ '\n')
         '''
+        print(data)  # отправка сообщений от sysloga в консоль для отладки.
 
-        # Parse
-        event = re.search('(?P<priority><\d{,3}>)(?P<date>\w{,3}\s+\d{,2}\s+\d{,2}:\d{,2}:\d{2,2})(?P<fromhost>\s+[^:]+){0,1}\s+(?P<process>\S+:)(?P<syslogtag>\s+\S+:){0,1}\s+(?P<message>.+)', data)
-        priority = event.group('priority').replace('<','').replace('>','')
-        timestamp = datetime.datetime.strptime(str(datetime.datetime.now().year) + ' ' + event.group('date'), '%Y %b %d %H:%M:%S')
-        devicereportedtime = timestamp.strftime('%Y-%m-%d %H:%M:%S')
-        fromhost = event.group('fromhost')
-        process = event.group('process')
-        syslogtag = event.group('syslogtag')
-        message = event.group('message')
-        cursor.execute("INSERT INTO syslog (priority,devicereportedtime,fromhost,process,syslogtag,message) VALUES (?,?,?,?,?,?)", (priority,devicereportedtime,fromhost,process,syslogtag,message))
-
-        db.commit()
-        
 
 if __name__ == '__main__':
     try:
-        db = sqlite3.connect('destination.db')
-        server = MyUDPServer((HOST, PORT), SyslogUDPHandler, db)
+        server = socketserver.UDPServer((HOST, PORT), SyslogUDPHandler)
         server.serve_forever(poll_interval=0.5)
+        print('Start server on: {}. Listening port:{}'.format(HOST, PORT))
     except (IOError, SystemExit):
         raise
     except KeyboardInterrupt:
+        db.close()
         print('Ctrl+C Pressed. Shutting down.')
