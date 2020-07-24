0b4400682cb0ab4190145a957821adab0f2764be
dim5x <dim5x@yahoo.com>
2020-07-24 20:32:27 +0300

    Создание табличек.

--- createDB.py
+++ createDB.py
@@ -1,11 +1,13 @@
 import sqlite3
 
-""" Создал табличку c именем syslog и полями: 
+""" Табличка c именем syslog и полями: 
                             id
                             priority = приоритет
+                            devicereportedtime = время с железки
+                            process = источник события
                             event_time = время события
                             fromhost = IP-адресс (доменное имя) 
-                            syslogtag = источник события
+                            syslogtag = тег события
                             message = сообщение о событии
                             mac_allow = разрешенные маки
                             mac_disallow = неразрешенные маки
@@ -17,8 +19,10 @@ cursor = db.cursor()  # создаём курсор
 # SQL- запрос на создание:
 cursor.execute('''CREATE TABLE IF NOT EXISTS syslog (id INTEGER PRIMARY KEY,
                         priority integer,
+                        devicereportedtime DATETIME, 
                         event_time DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%S','now','localtime')),
                         fromhost varchar(200),
+                        process text,
                         syslogtag varchar(50),
                         message varchar(400),
                         mac_allow varchar(17),
