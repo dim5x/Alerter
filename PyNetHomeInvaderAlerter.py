2e235bf1ad744ed334680d9f143f63618cfd195d
dim5x <dim5x@yahoo.com>
2020-07-25 19:58:00 +0300

    import os.

--- PyNetHomeInvaderAlerter.py
+++ PyNetHomeInvaderAlerter.py
@@ -2,11 +2,12 @@ import socketserver
 import sqlite3
 import re
 from datetime import datetime
+import os
 
 # Под виндой с 514-ым портом могут быть проблемы, нужно повышение привилегий.
 # Заменить тем, что выше 1023-его.
 # HOST, PORT = 'x.x.x.x', 514
-#HOST, PORT = '192.168.0.102', 5140
+# HOST, PORT = '192.168.0.102', 5140
 # my_branch test
 HOST, PORT = '172.27.0.165', 514
 
@@ -23,19 +24,21 @@ class SyslogUDPHandler(socketserver.BaseRequestHandler):
             r'(?P<priority><\d{,3}>)(?P<date>\w{,3}\s+\d{,2}\s+\d{,2}:\d{,2}:\d{2,2})(?P<from_host>\s+[^:]+){0,'
             r'1}\s+(?P<process>\S+:)(?P<syslog_tag>\s+\S+:){0,1}\s+(?P<message>.+)', data)
         priority = event.group('priority').strip('><')
-        timestamp = datetime.strptime(str(datetime.now().year) + ' ' + event.group('date'),
-                                               '%Y %b %d %H:%M:%S')
+
+        # Дубляж же?
+        # timestamp = datetime.strptime(str(datetime.now().year) + ' ' + event.group('date'), '%Y %b %d %H:%M:%S')
         device_time = datetime.strptime(str(datetime.now().year) + ' ' + event.group('date'), '%Y %b %d %H:%M:%S')
+
         if isinstance(event.group('from_host'), str):
             from_host = event.group('from_host')
         else:
-            from_host = self.client_address[0]       
+            from_host = self.client_address[0]
         process = event.group('process')
         syslog_tag = event.group('syslog_tag')
         message = event.group('message')
 
         cursor.execute(
-            "INSERT INTO syslog (priority, device_time, from_host, process, syslog_tag, message) "
+            "INSERT INTO syslog (priority, device_time, from_host, process, syslog_tag, message)"
             "VALUES (?,?,?,?,?,?)", (priority, device_time, from_host, process, syslog_tag, message))
 
         db.commit()
@@ -51,7 +54,7 @@ class SyslogUDPHandler(socketserver.BaseRequestHandler):
 if __name__ == '__main__':
     try:
         if not os.path.exists('destination.db'):
-            print('нужна база!')           
+            print('нужна база!')
         server = socketserver.UDPServer((HOST, PORT), SyslogUDPHandler)
         print('Start server on: {}. Listening port: {}'.format(HOST, PORT))
         server.serve_forever(poll_interval=0.5)
