ed05818c5d13a9e07b1d273b69a3a20f5fe166ab
Леонов Александр Владимирович <avl8@europlan.ru>
2020-07-25 19:31:45 +0300

    Скрипты для создания БД. Добавл db в gitignore

---
+++ cicd/current_state.sql
@@ -0,0 +1,7 @@
+create table current_state(
+	mac varchar(30),
+	state integer,
+	started_at datetime,
+	from_host varchar(200),
+	port varchar(30)
+	);
---
+++ cicd/variables.sql
@@ -0,0 +1,5 @@
+create table variables(
+	name varchar(30),
+	integer_value integer,
+	text_value varchar(30)
+	);
