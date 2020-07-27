268c8eb4379afc3a9a6e360c7fb97b036608deb5
Леонов Александр Владимирович <avl8@europlan.ru>
2020-07-27 10:48:40 +0300

    расширяем использование конфига

--- PyNetHomeInvaderAlerter.py
+++ PyNetHomeInvaderAlerter.py
@@ -7,14 +7,15 @@ import db_management
 import os
 import subprocess
 import sys
+import management
 
 
 # Под виндой с 514-ым портом могут быть проблемы, нужно повышение привилегий.
 # Заменить тем, что выше 1023-его.
 # HOST, PORT = 'x.x.x.x', 514
-#HOST, PORT = '192.168.0.102', 5140
-# my_branch test
-HOST, PORT = '172.27.0.165', 514
+
+HOST, PORT = management.get_option('alerter_host'), management.get_option('alerter_port')
+db_name = management.get_option('db_name')
 
 if not os.path.exists('destination.db'):
     print('Нужна база!')
@@ -50,11 +51,11 @@ class SyslogUDPHandler(socketserver.BaseRequestHandler):
         message = event.group('message')
 
         data_dic = {'priority' : priority, 'device_time' : device_time.strftime('%Y-%m-%d %H:%M:%S'), 'from_host' : from_host, 'process' : process, 'syslog_tag' : syslog_tag, 'message' : message}
-        #db_management.insert_data(data_dic, 'syslog', cursor)
+        db_management.insert_data(data_dic, 'syslog', cursor)
 
-        cursor.execute(
-            "INSERT INTO syslog (priority, device_time, from_host, process, syslog_tag, message) "
-            "VALUES (?,?,?,?,?,?)", (priority, device_time, from_host, process, syslog_tag, message))
+        #cursor.execute(
+        #    "INSERT INTO syslog (priority, device_time, from_host, process, syslog_tag, message) "
+        #    "VALUES (?,?,?,?,?,?)", (priority, device_time, from_host, process, syslog_tag, message))
 
         db.commit()
 
