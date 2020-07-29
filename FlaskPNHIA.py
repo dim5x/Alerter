86ba9c8fdce266faa3870cfda9874a84a0d679e0
Леонов Александр Владимирович <avl8@europlan.ru>
2020-07-29 15:06:50 +0300

    1. gitignore for visual studio 2 use_reloader option

--- FlaskPNHIA.py
+++ FlaskPNHIA.py
@@ -222,6 +222,6 @@ def login_admin():
 
 if __name__ == '__main__':
     flask_host = management.get_option('flask_host')
-    print(flask_host)
+    flask_use_reloader = management.get_option('flask_use_reloader')
     #
-    app.run(debug=True, use_reloader=True,host=flask_host)
+    app.run(debug=True, use_reloader=flask_use_reloader,host=flask_host)
