6482591975c710dbbf8c540eb392eeacefc3414b
dim5x <dim5x@yahoo.com>
2020-07-30 09:57:30 +0300

    Работа с admin таблицей.

--- management.py
+++ management.py
@@ -1,22 +1,22 @@
-import os 
+# import os
+
 
 # Считываем настройки
 # Локальные имеют приоритет над глобальными
 def get_option(name):
-    options  = {}
-    
+    options = {}
+
     with open('../local.config') as file:
         lines = file.read().splitlines()
         for line in lines:
-           key, value = line.split(':')
-           options.update({key:value})
+            key, value = line.split(':')
+            options.update({key: value})
 
-    with open ('global.config') as file:
+    with open('global.config') as file:
         lines = file.read().splitlines()
-
-    for line in lines:
-        key, value = line.split(':')
-        if not key in options:
-            options.update({key:value})
+        for line in lines:
+            key, value = line.split(':')
+            if key not in options:
+                options.update({key: value})
 
     return options[name]
