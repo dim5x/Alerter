82694ea64c64f4be298ae171a140abeec4301ee3
dim5x <dim5x@yahoo.com>
2020-09-19 03:26:18 +0300

    PEP257

--- static/DataTables/for_state_table.js
+++ static/DataTables/for_state_table.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     $('#state').DataTable({
         "lengthMenu": [[10, 20, 30, -1], [10, 20, 30, "All"]],
-        "order": [[ 0, "desc" ]]
+        "order": [[0, "desc"]]
     });
-})
-
+});
