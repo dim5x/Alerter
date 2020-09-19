a35efe085bf463edb3bfcf6055cfdb5e8b7cd1de
dim5x <dim5x@yahoo.com>
2020-09-19 03:55:14 +0300

    PEP257

--- cicd/test/unit_test.py
+++ cicd/test/unit_test.py
@@ -27,7 +27,8 @@ class FlaskAppTests(unittest.TestCase):
     def test_post_hello_endpoint(self):
         """Проверка на отклик.
 
-         Должен выдать код 200 у главной страницы, при успешном запуске Фласка."""
+         Должен выдать код 200 у главной страницы, при успешном запуске Фласка.
+         """
         resp = self.app.get('/')
         self.assertEqual(resp.status_code, 200)
 
