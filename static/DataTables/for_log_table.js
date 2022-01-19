1253ad5ac06c5968a1557a4b37bfbe9aea3c6c10
dim5x <dim5x@yahoo.com>
2022-01-20 02:29:09 +0300

    Тест ajax.

--- static/DataTables/for_log_table.js
+++ static/DataTables/for_log_table.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
     $("#log").DataTable({
+        // ajax: '/test',
+        // columns:[
+        //     {data: 'receivedat'},
+        //     {data: 'priority'},
+        //     {data: 'from_host'},
+        //     {data: 'process'},
+        //     {data: 'syslog_tag'},
+        //     {data: 'mac_type'},
+        //     {data: 'message'},
+        //     {data: 'mac'},
+        // ],
         "lengthMenu": [[50, 75, 100, -1], [50, 75, 100, "All"]],
         "order": [[ 0, "desc" ]]
     });
